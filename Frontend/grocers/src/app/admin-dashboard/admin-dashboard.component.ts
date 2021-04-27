f4dc6c6d6cc747b9f820db491062ad9fe7dd0e57
Your Name <you@example.com>
2021-04-26 21:43:29 -0400

    participant 1 done

--- Frontend/grocers/src/app/admin-dashboard/admin-dashboard.component.ts
+++ Frontend/grocers/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminDashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router:Router) { }
 
   ngOnInit(): void {
   }
 
+  homePage(){
+    this.router.navigate(["/admin"])
+  }
+
+  addProduct(){
+    this.router.navigate(["/addProduct"])
+  }
+
+  deleteProduct(){
+    this.router.navigate(["/deleteProduct"])
+  }
+
+  updateProduct(){
+    this.router.navigate(["/productUpdate"])
+  }
+
+  viewRequest(){
+    this.router.navigate(["/employeeViewRequest"])
+  }
 }
